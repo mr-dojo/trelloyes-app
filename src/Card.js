413465b764ebe3f5ffd4949d8f9d2279894217d8
Carlo Costantino <costantinoart@gmail.com>
2019-10-31 11:24:41 -1000

    Finished formating Card component

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Card(props) {
+  return (
+    <div className='Card'>
+      <button type='button'>Delete</button>
+      <h3>{props.title}</h3>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
+export default Card;
