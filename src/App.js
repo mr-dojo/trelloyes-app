c2fe7158931ac96a07c6006fcd2b13f3fe745819
Carlo Costantino <costantinoart@gmail.com>
2019-10-31 11:20:16 -1000

    Finished first iteration of App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import List from './List.js';
+import './App.css';
 
-function App() {
-  return (
+class App extends Comment {
+  static defaultProps = {
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
   
-  );
+  render () {
+    const { store } = this.props
+    return(
+      <main className='App'>
+        <header className='App-header'>
+          <h1>Trelloys!</h1>
+          <div className='App-list'>
+            {store.lists.map(list => (
+              <List 
+                key={list.id}
+                header={list.header}
+                cards={list.cardIds.map(id => store.allCards[id])}
+              />
+            ))}
+          </div>
+        </header>
+      </main>
+    );
+  }
 }
 
 export default App;
