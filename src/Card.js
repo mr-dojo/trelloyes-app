f9a31781fe5c017c7877f061ab986dd7a1926d79
Carlo Costantino <costantinoart@gmail.com>
2019-10-31 11:27:54 -1000

    Small tweaks to format

--- src/Card.js
+++ src/Card.js
@@ -3,11 +3,14 @@ import React from 'react';
 function Card(props) {
   return (
     <div className='Card'>
-      <button type='button'>Delete</button>
+      <button 
+        type='button'
+      >
+        Delete
+      </button>
       <h3>{props.title}</h3>
       <p>{props.content}</p>
     </div>
   )
 }
-
 export default Card;
