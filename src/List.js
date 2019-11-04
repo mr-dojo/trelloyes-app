526bb08b2e79772564ce6ea827a1b0c826fed833
Carlo Costantino <costantinoart@gmail.com>
2019-11-04 08:22:35 -1000

    Added example project code and worked through how it works

--- src/List.js
+++ src/List.js
@@ -1,17 +1,27 @@
 import React from 'react';
-// import Card from './Card.js';
+import Card from './Card';
+import './List.css';
 
 export default function List(props) {
-
   return (
     <section className='List'>
       <header className='List-header'>
         {props.header}
       </header>
       <div className='List-cards'>
-        {props.cards}
+        {props.cards.map((card) => 
+          <Card
+            key={card.id}
+            title={card.title}
+            content={card.content}
+            />
+        )}
+        <button 
+          type='button'
+        >
+          + Add Random Card
+        </button> 
       </div>
     </section>
   )
 }
-// return an array of card components
