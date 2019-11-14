1a67ce571c3b2d7c734f2e71e7c4f5ea3336e438
Carlo Costantino <costantinoart@gmail.com>
2019-11-14 11:29:10 -1000

    Changed List to a class component and added Add Random Card callback prop

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ export default class App extends Component {
   }
   
   render () {
-    const { store } = this.props
     return(
       <main className='App'>
         <header className='App-header'>
--- src/List.js
+++ src/List.js
@@ -2,26 +2,33 @@ import React from 'react';
 import Card from './Card';
 import './List.css';
 
-export default function List(props) {
-  return (
-    <section className='List'>
-      <header className='List-header'>
-        {props.header}
-      </header>
-      <div className='List-cards'>
-        {props.cards.map((card) => 
-          <Card
-            key={card.id}
-            title={card.title}
-            content={card.content}
-            />
-        )}
-        <button 
-          type='button'
-        >
-          + Add Random Card
-        </button> 
-      </div>
-    </section>
-  )
+export default class List extends React.Component {
+  static defaultProps = {
+    header: '',
+    cards: [],
+  }
+  render() {
+    return (
+      <section className='List'>
+        <header className='List-header'>
+          {this.props.header}
+        </header>
+        <div className='List-cards'>
+          {this.props.cards.map((card) => 
+            <Card
+              key={card.id}
+              title={card.title}
+              content={card.content}
+              />
+          )}
+          <button 
+            type='button'
+            onClick={() => this.props.onCreateRandom()}
+          >
+            + Add Random Card
+          </button> 
+        </div>
+      </section>
+    )
+  }
 }
