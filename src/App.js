299eb05871ab51bf43a262566eae0d74c5e3a2a1
Carlo Costantino <costantinoart@gmail.com>
2019-11-14 11:09:40 -1000

    Added state and functional callback props for Delete and Add Random

--- src/App.js
+++ src/App.js
@@ -2,13 +2,54 @@ import React, { Component } from 'react';
 import List from './List.js';
 import './App.css';
 
-class App extends Component {
-  static defaultProps = {
-    store: {
-      lists: [],
-      allCards: {},
-    }
-  };
+export default class App extends Component {
+  state = {
+    lists: [
+      {
+        id: '1',
+        header: 'First list',
+        cardIds: [ 'a', 'b', 'e', 'f', 'g', 'j', 'l', 'm' ],
+      },
+      {
+        id: '2',
+        header: 'Second list',
+        cardIds: ['b', 'c', 'd', 'f', 'h', 'i', 'k'],
+      },
+      {
+        id: '3',
+        header: 'Third list',
+        cardIds: [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm' ],
+      },
+      {
+        id: '4',
+        header: 'Fourth list',
+        cardIds: [ 'l', 'm' ],
+      },
+    ],
+    allCards: {
+      'a': { id: 'a', title: 'First card', content: 'lorem ipsum' },
+      'b': { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+      'c': { id: 'c', title: 'Third card', content: 'lorem ipsum' },
+      'd': { id: 'd', title: 'Fourth card', content: 'lorem ipsum' },
+      'e': { id: 'e', title: 'Fifth card', content: 'lorem ipsum' },
+      'f': { id: 'f', title: 'Sixth card', content: 'lorem ipsum' },
+      'g': { id: 'g', title: 'Seventh card', content: 'lorem ipsum' },
+      'h': { id: 'h', title: 'Eighth card', content: 'lorem ipsum' },
+      'i': { id: 'i', title: 'Ninth card', content: 'lorem ipsum' },
+      'j': { id: 'j', title: 'Tenth card', content: 'lorem ipsum' },
+      'k': { id: 'k', title: 'Eleventh card', content: 'lorem ipsum' },
+      'l': { id: 'l', title: 'Twelfth card', content: 'lorem ipsum' },
+      'm': { id: 'm', title: 'Thirteenth card', content: 'lorem ipsum' },
+    },
+  }
+
+  handleDeleteCard = (id) => {
+    console.log('Delete card with id', {id})
+  }
+
+  handleCreateRandomCard = () => {
+    console.log('Create random card')
+  }
   
   render () {
     const { store } = this.props
@@ -17,11 +58,13 @@ class App extends Component {
         <header className='App-header'>
           <h1>Trelloys!</h1>
           <div className='App-list'>
-            {store.lists.map(list => (
+            {this.state.lists.map(list => (
               <List 
                 key={list.id}
                 header={list.header}
-                cards={list.cardIds.map(id => store.allCards[id])}
+                cards={list.cardIds.map(id => this.state.allCards[id])}
+                onDeleteCard={this.handleDeleteCard}
+                onCreateRandom={this.handleCreateRandomCard}
               />
             ))}
           </div>
@@ -31,4 +74,3 @@ class App extends Component {
   }
 }
 
-export default App;
